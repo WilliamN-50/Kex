fe6209dd303aa7486cf44f657e32768dd0ba550b
sirui <sirui980116@gmail.com>
2022-03-16 11:06:34 +0100

    Add plot relative error for plot

--- fix_step_v2/Euler_diff_stepsize.py
+++ fix_step_v2/Euler_diff_stepsize.py
@@ -20,30 +20,31 @@ class Diff_eq_2(gd.DifferentialEquation):
         return 3/2 * y/(t+1) + np.sqrt(t+1)
 
 
-def euler_method(nn_tr_te, device, t0, y0, h0, tol, diff_eq, num_step):
+def adaptive_euler_method(nn_tr_te, device, t0, y0, h0, tol, diff_eq, num_step):
     t = np.array([t0])
     y = np.array([y0])
-    y_euler = np.array([y0])
     h = np.array([h0])
-    t_step = 0
     for i in range(num_step):
         data = torch.tensor([t[i], t[i]+h[i], h[i], y[i]]).to(device).float()
         nn_e = nn_tr_te.model(data).cpu()
         nn_e = nn_e.detach().numpy()
         tao = nn_e*h[i]**2/2
-        # print(h)
-        # print([0.9*h[i]*min(max((tol/(2*tao)**(1/2), 0.3)), 2)])
         h = np.append(h, 0.9*h[i]*min(max((tol/(2*tao)**(1/2), 0.3)), 2))
-        # print(h)
         t = np.append(t, t[i]+h[i+1])  # t[i+1]
         y = np.append(y, y[i] + h[i+1]*diff_eq.func(t[i], y[i]) + h[i+1]**2 * nn_e)  # y[i+1]
-        y_euler = np.append(y_euler, y[i] + 0.1*diff_eq.func(t_step, y[i]))
-        t_step += 0.1
-    return t, y, y_euler
+    return t, y
+
+
+def fix_euler(t, y0, step_size, diff_eq):
+    y = np.array([y0])
+    for i in range(t.shape[0]):
+        y = np.append(y, y[i] + step_size*diff_eq.func(t[i], y[i]))
+    return y
 
 
 def main():
     t = np.arange(0, 10, 0.1)
+    # print(t.shape[0])
     diff_eq = Diff_eq_2(0, 10, [1])
     data_integrate = diff_eq.integrate(t_points=t)
     data_input = diff_eq.reshape_data(data_integrate)
@@ -59,8 +60,18 @@ def main():
         nn_tr_te.nn_train()
         nn_tr_te.nn_test()
 
-    t_pred, y_pred, y_euler = euler_method(nn_tr_te=nn_tr_te, device=device, t0=0, y0=1, h0=0.5, tol=0.1, diff_eq=diff_eq, num_step=100)
+    t_pred, y_pred = adaptive_euler_method(nn_tr_te=nn_tr_te, device=device, t0=0, y0=1, h0=0.5, tol=0.1, diff_eq=diff_eq, num_step=100)
+    y_fix_euler = fix_euler(t=t, y0=1, step_size=0.1, diff_eq=diff_eq)
+    y_ref = diff_eq.integrate(t_points=t_pred)
 
+    ref_error_nn = np.abs(y_pred - y_ref[:, 1])
+    ref_euler = np.abs(y_fix_euler[:-1] - data_integrate[:, 1])
+    plt.plot(t_pred, ref_error_nn, label="relative error for prediction")
+    plt.plot(t, ref_euler, label="relative error for euler")
+    plt.legend()
+    plt.show()
+
+    """ # t_pred with fix step size.
     t2 = np.arange(0, 10, 0.15)
     y = np.zeros(t2.shape)
     y[0] = 1
@@ -71,14 +82,20 @@ def main():
         nn_e = nn_tr_te.model(data).cpu()
         nn_e = nn_e.detach().numpy()
         y[i + 1] = y[i] + h * diff_eq.func(t2[i], y[i]) + h ** 2 * nn_e
+    """
 
-    plt.plot(t_pred, y_pred, label='prediction')
-    plt.plot(t, y_euler[:-1], label='Euler')
-    plt.plot(t2, y, label="nntest")
-    plt.plot(data_integrate[:, 0], data_integrate[:, 1], label='target')
+    # plt.plot(t_pred, y_pred, label='prediction')
+    # plt.plot(t, y_euler[:-1], label='Euler')
+    # plt.plot(t2, y, label="nntest")
+    # plt.plot(t_pred, y_ref[:, 1], label='target')
     # plt.plot(data_integrate[:, 0], data_integrate[:, 2], label='target')
-    plt.legend()
-    plt.show()
+    # plt.legend()
+    # plt.show()
+
+    # print(y_pred)
+    # print(y_ref)
+    # print(y_euler)
+
 
 if __name__ == '__main__':
     main()
