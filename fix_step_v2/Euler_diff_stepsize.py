e368dbeb0e15fd2fe64988a685aaef7166032f16
Sirui Li <97970597+LI-Sirui@users.noreply.github.com>
2022-03-25 14:55:27 +0100

    Update plot

--- fix_step_v2/Euler_diff_stepsize.py
+++ fix_step_v2/Euler_diff_stepsize.py
@@ -3,7 +3,6 @@ import numpy as np
 import GenerateData as gd
 import NN_model
 import torch
-import GenerateData as gd
 
 
 def fix_euler_method(model, t0, t_end, y0, h, diff_eq, device, deep=True):
@@ -33,10 +32,9 @@ def fix_euler_method(model, t0, t_end, y0, h, diff_eq, device, deep=True):
 def adaptive_euler(model, t0, t_end, y0, h, tol, diff_eq, device):
     t = [t0]
     y = [y0]
-    h_list= []
+    h_list = []
 
     i = 0
-
     while t[-1] < t_end - h:
         norm, nn_e = comp_norm(model, t[-1], y[-1], h, diff_eq, device)
         if norm < tol:
@@ -59,6 +57,7 @@ def adaptive_euler(model, t0, t_end, y0, h, tol, diff_eq, device):
     y = np.array(y)
     return t, y, i, h_list
 
+
 def comp_norm(model, t0, y0, h, diff_eq, device):
     temp0 = np.array([y0[j] for j in range(diff_eq.num_y)])
     temp1 = np.array([t0, t0 + h])
@@ -71,22 +70,40 @@ def comp_norm(model, t0, y0, h, diff_eq, device):
     return norm, nn_e
 
 
+def plot_diagram(num_y, x, y, label_title, mark):
+    for i in range(num_y):
+        plt.plot(x, y[:, i], mark, label=label_title + str(i + 1), markerfacecolor='none')
+
+
+def comp_rel_error(num_y, y_prediction, y_target):
+    rel_error = np.empty((y_prediction.shape[0], y_prediction.shape[1]))
+    for i in range(num_y):
+        rel_error[:, i] = np.abs(y_prediction[:, i] - y_target[:, i + 1])
+
+    return rel_error
+
+
 def main():
-    y0 = [1, 2]
+    y0 = [0.5, 0, 0, np.sqrt(3)]
+    # y0=[1, 2]
     t0 = 0
     t_end = 25
-    diff_eq = gd.VanDerPol(t0, t_end, y0)
+    tol = 0.0001
+    h_start = 0.1
+    diff_eq = gd.Kepler(t_0=t0, t_end=t_end, y_0=y0)
+    # diff_eq = gd.LodkaVolterra(t0, t_end, y0)
 
     device = "cpu"
     model = NN_model.NeuralNetwork(diff_eq.num_y)
-    # model.load_state_dict(torch.load("../trained_model/eq_van_der_model_Adam_1_2_1000p_noise01.pth"))
-    model.load_state_dict(torch.load("eq_van_der_model_Adam_no_noise_1_2_1000p_100ep_lr5_10_4.pth"))
+    model.load_state_dict(torch.load("../trained_model/Kepler_no_noise_0_10_1000p_75ep.pth"))
+    # model.load_state_dict(torch.load("../trained_model/eq_1_model_50_adam_batch.pth"))
+    # model.load_state_dict(torch.load("eq_van_der_model_Adam_no_noise_1_2_1000p_100ep_lr5_10_4.pth"))
     model.eval()
 
-    t_dem_adap, y_dem_adap, i, h_list = adaptive_euler(model=model, t0=t0, t_end=t_end, y0=y0, h=0.1, tol=0.0001, diff_eq=diff_eq,
-                                            device=device)
-    # print(y_dem_adap)
+    t_dem_adap, y_dem_adap, i, h_list = adaptive_euler(model=model, t0=t0, t_end=t_end, y0=y0, h=h_start, tol=tol,
+                                                       diff_eq=diff_eq, device=device)
     h = np.min(h_list)
+    # h = 25/(len(t_dem_adap))
     t_dem_fix, y_dem_fix = fix_euler_method(model=model, t0=t0, t_end=t_end, y0=y0, h=h, diff_eq=diff_eq,
                                             device=device)
     # print(y_dem_fix)
@@ -101,42 +118,25 @@ def main():
     data_integrate = diff_eq.integrate(t_points=t_dem_fix)
     y_rel_dem_adap = diff_eq.integrate(t_points=t_dem_adap)
 
-    rel_e_dem_adap1 = np.abs(y_dem_adap[:, 0] - y_rel_dem_adap[:, 1])
-    rel_e_dem_adap2 = np.abs(y_dem_adap[:, 1] - y_rel_dem_adap[:, 2])
+    rel_e_dem_adap = comp_rel_error(diff_eq.num_y, y_dem_adap, y_rel_dem_adap)
+    rel_e_dem_fix = comp_rel_error(diff_eq.num_y, y_dem_fix, data_integrate)
+    rel_e_euler_fix = comp_rel_error(diff_eq.num_y, y_euler_fix, data_integrate)
 
-    rel_e_dem_fix1 = np.abs(y_dem_fix[:, 0] - data_integrate[:, 1])
-    rel_e_dem_fix2 = np.abs(y_dem_fix[:, 1] - data_integrate[:, 2])
-
-    rel_e_euler_fix1 = np.abs(y_euler_fix[:, 0] - data_integrate[:, 1])
-    rel_e_euler_fix2 = np.abs(y_euler_fix[:, 1] - data_integrate[:, 2])
-
-    # ref_euler = np.abs(y_fix_euler[:-1] - data_integrate[:, 1])
-    plt.plot(t_dem_adap, rel_e_dem_adap1, label="DEM adaptive y1")
-    plt.plot(t_dem_adap, rel_e_dem_adap2, label="DEM adaptive y2")
-
-    plt.plot(t_dem_fix, rel_e_dem_fix1, "--", label="DEM fix y1")
-    plt.plot(t_dem_fix, rel_e_dem_fix2, "--", label="DEM fix y2")
-
-    plt.plot(t_euler_fix, rel_e_euler_fix1, ".", label="Euler fix y1")
-    plt.plot(t_euler_fix, rel_e_euler_fix2, ".", label="Euler fix y2")
+    plot_diagram(diff_eq.num_y, t_dem_adap, rel_e_dem_adap, "DEM adaptive y", "")
+    plot_diagram(diff_eq.num_y, t_dem_fix, rel_e_dem_fix, "DEM fix y", "--")
+    plot_diagram(diff_eq.num_y, t_euler_fix, rel_e_euler_fix, "Euler fix y", ".")
     plt.title("Relative error")
     plt.legend()
     plt.show()
 
-    plt.plot(t_dem_adap, y_dem_adap[:, 0], "o--", markerfacecolor='none', label='DEM adaptive y1')
-    plt.plot(t_dem_adap, y_dem_adap[:, 1], "o--", markerfacecolor='none', label='DEM adaptive y2')
-
-    plt.plot(t_dem_fix, y_dem_fix[:, 0], "--", label="DEM fix y1")
-    plt.plot(t_dem_fix, y_dem_fix[:, 1], "--", label="DEM fix y2")
-
-    plt.plot(t_euler_fix, y_euler_fix[:, 0], label="Euler fix y1")
-    plt.plot(t_euler_fix, y_euler_fix[:, 1], label="Euler fix y2")
-
-    plt.plot(data_integrate[:, 0], data_integrate[:, 1], label='Exact y1')
-    plt.plot(data_integrate[:, 0], data_integrate[:, 2], label='Exact y2')
+    plot_diagram(diff_eq.num_y, t_dem_adap, y_dem_adap, label_title="DEM adaptive y", mark="o--")
+    plot_diagram(diff_eq.num_y, t_dem_fix, y_dem_fix, label_title="DEM fix y", mark="--")
+    plot_diagram(diff_eq.num_y, data_integrate[:, 0], data_integrate[:, 1:], label_title="Exact y", mark="")
     plt.title("Solve of the ODE")
     plt.legend()
     plt.show()
+
+
 
 
 if __name__ == '__main__':
