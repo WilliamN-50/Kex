0ead688849a4a5ae80c49a577fed06a5e361a758
WilliamN-50 <92812207+WilliamN-50@users.noreply.github.com>
2022-04-29 14:33:58 +0200

    Update plot_solution.py

--- neural_network/plot_diffeq/plot_solution.py
+++ neural_network/plot_diffeq/plot_solution.py
@@ -7,6 +7,11 @@ from neural_network.model import differentialequations as deq
 
 
 def euler_method(model, t_0, t_end, y_0, h, diff_eq, deep=True):
+    """
+    ____________________________
+    Solves the IVP using Euler method or deep Euler method.
+    ____________________________
+    """
     t = np.arange(t_0, t_end, h)
     y = np.empty((len(t), diff_eq.num_y))
     y[0, :] = y_0
@@ -22,6 +27,11 @@ def euler_method(model, t_0, t_end, y_0, h, diff_eq, deep=True):
 
 
 def adaptive_euler(model, t_0, t_end, y_0, h, tol, diff_eq):
+    """
+    ____________________________
+    Solves the IVP using adaptive Euler method.
+    ____________________________
+    """
     t = [t_0]
     y = [y_0]
     h_list = []
@@ -70,6 +80,11 @@ def _secant_method(y_1, y_2, func, tol=0.01, max_iter=1000):
 
 
 def implicit_euler(model, t_0, t_end, y_0, h, diff_eq, tol=0.01, max_iter=1000, deep=True):
+    """
+    ____________________________
+    Solves the IVP using implicit Euler or deep implicit Euler method.
+    ____________________________
+    """
     t = np.arange(t_0, t_end, h)
     y = np.empty((len(t), diff_eq.num_y))
     y[0, :] = y_0
@@ -96,6 +111,11 @@ def implicit_euler(model, t_0, t_end, y_0, h, diff_eq, tol=0.01, max_iter=1000, 
 
 
 def adaptive_implicit_euler(model, t_0, t_end, y_0, h, tol, diff_eq, secant_tol=0.01, max_iter=1000):
+    """
+    ____________________________
+    Solves the IVP using adaptive implicit Euler.
+    ____________________________
+    """
     t = [t_0]
     y = [y_0]
     h_list = []
@@ -138,12 +158,22 @@ def adaptive_implicit_euler(model, t_0, t_end, y_0, h, tol, diff_eq, secant_tol=
 
 
 def comp_norm(model, t, h, y, func):
+    """
+    ____________________________
+    Computes the norm for the adaptive step-size method.
+    ____________________________
+    """
     nn_e = calc_nn_lte(model, t, h, y, func)
     norm = np.amax(np.abs(nn_e)) * h**2
     return norm, nn_e
 
 
 def calc_nn_lte(model, t, h, y, func):
+    """
+    ____________________________
+    Computes the output of NeuralNetworkModel.
+    ____________________________
+    """
     if type(model) == model1.NeuralNetworkModel1:
         data = np.concatenate(([t, t+h], y), axis=0)
     else:
@@ -167,16 +197,16 @@ def plot_diagram(t, y, num_y, label, marker):
         plt.plot(t, y[:, i], marker, label=label + str(i+1), markerfacecolor='none')
 
 
-def plot_diagram_hamiltonian(t, y, num_y, label, marker, title_p=" p", title_q=" q", log=False):
+def plot_diagram_hamiltonian(t, y, num_y, label, marker, title_p="p", title_q="q", log=False):
     """
     ____________________________
-    Plots the absolut error for a hamiltonian system.
+    Plots the absolute error for a hamiltonian system.
     ____________________________
     """
     plt.subplot(1, 2, 1)
     for i in range(num_y//2):
         plt.plot(t, y[:, i], marker, label=label + title_q + str(i+1))
-    plt.title("Absolut Error of" + title_q)
+    plt.title("Absolute Error of " + title_q)
     plt.xlabel("t")
     plt.ylabel("Error")
     plt.legend()
@@ -186,7 +216,7 @@ def plot_diagram_hamiltonian(t, y, num_y, label, marker, title_p=" p", title_q="
     plt.subplot(1, 2, 2)
     for i in range(num_y//2, num_y):
         plt.plot(t, y[:, i], marker, label=label + title_p + str(i+1-num_y//2))
-    plt.title("Absolut Error of" + title_p)
+    plt.title("Absolute Error of " + title_p)
     plt.xlabel("t")
     plt.ylabel("Error")
     plt.legend()
@@ -195,6 +225,11 @@ def plot_diagram_hamiltonian(t, y, num_y, label, marker, title_p=" p", title_q="
 
 
 def plot_diagram_phase_2d(y, label, marker, title_p=" p", title_q=" q"):
+    """
+    ____________________________
+    Plots a phase diagram.
+    ____________________________
+    """
     plt.subplot(1, 2, 1)
     plt.plot(y[:, 0], y[:, 1], marker, label=label + title_q)
     plt.title(title_q)
@@ -211,26 +246,35 @@ def plot_diagram_phase_2d(y, label, marker, title_p=" p", title_q=" q"):
 
 
 def comp_abs_error(y_prediction, y_target):
+    """
+    ____________________________
+    Computes the absolute error of the prediction and target.
+    ____________________________
+    """
     rel_error = np.abs(y_prediction - y_target)
     return rel_error
 
 
 def comp_rel_error(y_prediction, y_target):
+    """
+    ____________________________
+    Computes the relative error of the prediction and target.
+    ____________________________
+    """
     rel_error = np.divide(np.abs(y_prediction - y_target), np.abs(y_target))
     return rel_error
 
 
+#Example
 def main():
     # Properties of differential equation
     t_0 = 0
     t_end = 30
-    # y_0 = [1, 2]
-    # diff_eq = deq.VanDerPol(t_0, t_end, y_0)
     y_0 = [0.5, 0, 0, np.sqrt(3)]
     diff_eq = deq.Kepler(t_0, t_end, y_0)
 
     # Load model
-    filename = "test.pth"
+    filename = "model2_lte.pth"
     model = model2.NeuralNetworkModel2(diff_eq.num_y)
     model.load_state_dict(torch.load(filename))
     model.eval()
@@ -241,19 +285,19 @@ def main():
     t_dem_adap, y_dem_adap, h, n_comp = adaptive_euler(model=model, t_0=t_0, t_end=t_end, y_0=y_0, h=h_0, tol=tol,
                                                        diff_eq=diff_eq)
     adap_data = diff_eq.integrate(t_points=t_dem_adap)
-    t_Idem_adap, y_Idem_adap, Ih, In_comp = adaptive_implicit_euler(model=model, t_0=t_0, t_end=t_end, y_0=y_0, h=h_0, tol=tol,
+    t_idem_adap, y_idem_adap, ih, in_comp = adaptive_implicit_euler(model=model, t_0=t_0, t_end=t_end, y_0=y_0, h=h_0, tol=tol,
                                                        diff_eq=diff_eq)
-    Iadap_data = diff_eq.integrate(t_points=t_Idem_adap)
+    iadap_data = diff_eq.integrate(t_points=t_idem_adap)
 
 
     # Construct data (Fix)
-    # h_fix = min(h)  # Compare with adaptive step-size method
-    h_fix = h_0  # Not compare with adaptive step-size method
+    h_fix = min(h)  # When comparing with adaptive step-size method
+    # h_fix = h_0  # When not comparing with adaptive step-size method
     t_dem_fix, y_dem_fix = euler_method(model=model, t_0=t_0, t_end=t_end, y_0=y_0, h=h_fix, diff_eq=diff_eq,
                                         deep=True)
-    t_Idem_fix, y_Idem_fix = implicit_euler(model=model, t_0=t_0, t_end=t_end, y_0=y_0, h=h_fix, diff_eq=diff_eq,
+    t_idem_fix, y_idem_fix = implicit_euler(model=model, t_0=t_0, t_end=t_end, y_0=y_0, h=h_fix, diff_eq=diff_eq,
                                         deep=True)
-    Ifix_data = diff_eq.integrate(t_points=t_Idem_fix)
+    ifix_data = diff_eq.integrate(t_points=t_idem_fix)
     t_euler_fix, y_euler_fix = euler_method(model=model, t_0=t_0, t_end=t_end, y_0=y_0, h=h_fix, diff_eq=diff_eq,
                                             deep=False)
     fix_data = diff_eq.integrate(t_points=t_dem_fix)
@@ -262,14 +306,14 @@ def main():
     print("t DEM adap", len(t_dem_adap))
     print("t DEM fix", len(t_dem_fix))
     print("t Euler fix", len(t_euler_fix))
-    print("t Implicit fix", len(t_Idem_fix))
+    print("t Implicit fix", len(t_idem_fix))
 
     # Construct relative data
     rel_dem_adap = comp_abs_error(y_dem_adap, adap_data[:, 1:])
     rel_dem_fix = comp_abs_error(y_dem_fix, fix_data[:, 1:])
     # rel_euler_fix = comp_abs_error(y_euler_fix, fix_data[:, 1:])
-    rel_Idem_fix = comp_abs_error(y_Idem_fix, Ifix_data[:, 1:])
-    rel_Idem_adap = comp_abs_error(y_Idem_adap, Iadap_data[:, 1:])
+    rel_idem_fix = comp_abs_error(y_idem_fix, ifix_data[:, 1:])
+    rel_idem_adap = comp_abs_error(y_idem_adap, iadap_data[:, 1:])
 
     # plot_diagram(t_dem_adap, rel_dem_adap, diff_eq.num_y, label="DEM adaptive y", marker="-")
     # plot_diagram(t_dem_fix, rel_dem_fix, diff_eq.num_y, label="DEM fix y", marker="--")
@@ -287,18 +331,17 @@ def main():
 
     # Use for Kepler
     plot_diagram_hamiltonian(t_dem_fix, rel_dem_fix, diff_eq.num_y, "DEM fix", marker="-", log=True)
-    plot_diagram_hamiltonian(t_Idem_fix, rel_Idem_fix, diff_eq.num_y, "DIEM fix", marker="--", log=True)
+    plot_diagram_hamiltonian(t_idem_fix, rel_idem_fix, diff_eq.num_y, "DIEM fix", marker="--", log=True)
     # plot_diagram_hamiltonian(t_dem_adap, rel_dem_adap, diff_eq.num_y, "DEM adaptive", marker="-", log=True)
     # plot_diagram_hamiltonian(t_Idem_adap, rel_Idem_adap, diff_eq.num_y, "DIEM adap", marker="--", log=True)
     plt.show()
 
     plot_diagram_phase_2d(y_dem_fix, "DEM fix", marker="--")
-    plot_diagram_phase_2d(y_Idem_fix, "DIEM fix", marker="--.")
+    plot_diagram_phase_2d(y_idem_fix, "DIEM fix", marker="--.")
     # plot_diagram_phase_2d(y_dem_adap, "DEM adaptive", marker="--")
     # plot_diagram_phase_2d(y_Idem_adap, "DIEM adaptive", marker="--.")
     plot_diagram_phase_2d(fix_data[:, 1:], "Reference", marker="")
     plt.show()
-    print(sum(h)/len(h))
 
 
 if __name__ == '__main__':
